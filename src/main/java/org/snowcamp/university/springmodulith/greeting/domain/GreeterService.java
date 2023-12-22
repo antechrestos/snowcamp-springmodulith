5405cb55610da8769ca4c96d8ffbb489131a696d
Ben Einaudi <antechrestos@gmail.com>
2023-12-22 23:44:39 +0100

    step 2 - user Async in conjunction with EnableAsync to isolate greetings service

--- src/main/java/org/snowcamp/university/springmodulith/greeting/domain/GreeterService.java
+++ src/main/java/org/snowcamp/university/springmodulith/greeting/domain/GreeterService.java
@@ -2,6 +2,7 @@ package org.snowcamp.university.springmodulith.greeting.domain;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
@@ -19,6 +20,7 @@ public class GreeterService {
     }
 
     @Transactional(propagation = Propagation.REQUIRES_NEW)
+    @Async
     public void greet(String orderId) {
         LOGGER.debug("greet - {}", orderId);
         try {
