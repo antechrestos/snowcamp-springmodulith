6c62f99e8be67312bec0efa1dd579ad9513b1d78
Ben Einaudi <antechrestos@gmail.com>
2023-12-23 00:44:56 +0100

    step 11 - documentation

--- src/test/java/org/snowcamp/university/springmodulith/ChartreuseShopApplicationTest.java
+++ src/test/java/org/snowcamp/university/springmodulith/ChartreuseShopApplicationTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.modulith.core.ApplicationModules;
+import org.springframework.modulith.docs.Documenter;
 
 public class ChartreuseShopApplicationTest {
 
@@ -17,5 +18,16 @@ public class ChartreuseShopApplicationTest {
     @Test
     void check_modularity(){
         ApplicationModules.of(ChartreuseShopApplication.class).verify();
+    }
+
+    @Test
+    void generateDocumentation() {
+        ApplicationModules modules = ApplicationModules.of(ChartreuseShopApplication.class);
+        Documenter.CanvasOptions canvasOptions = Documenter.CanvasOptions.defaults()
+                .revealInternals();
+        Documenter.DiagramOptions diagramOptions = Documenter.DiagramOptions.defaults()
+                .withStyle(Documenter.DiagramOptions.DiagramStyle.C4);
+        new Documenter(modules)
+                .writeDocumentation(diagramOptions, canvasOptions);
     }
 }
