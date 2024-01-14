7d9b49ef224bd204ad566649fcc761d6465d4847
Ben Einaudi <antechrestos@gmail.com>
2024-01-14 18:38:08 +0100

    [infrastructure] remove remaining spring-rabbit dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,7 +23,6 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.springframework.boot:spring-boot-starter-actuator")
 	implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
-	implementation("org.springframework.amqp:spring-rabbit")
 
 	implementation("io.micrometer:micrometer-tracing-bridge-otel")
 
